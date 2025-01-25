7f5ca50f8760a6bbcc08e6cf9beb59573bbf517f
kabsmuuka <kabwalimuuka@gmail.com>
2025-01-25 12:16:55 +0200

    some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
+import pg from "pg";
+
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("buyany", "postgres", "code", {
   host: "localhost",
   dialect: "postgres",
   logging: false,
+  dialectModule: pg,
 });
 
 sequelize
